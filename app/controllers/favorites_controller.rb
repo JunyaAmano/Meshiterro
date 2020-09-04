6b8b41343e43edf9008f57091836ce2362586cba
EC2 Default User <ec2-user@ip-172-31-32-188.ec2.internal>
2020-09-04 10:47:08 +0000

    [Finish-4-5]マイページへの遷移画面が不完全

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,6 +7,7 @@ class FavoritesController < ApplicationController
   end
 
 
+ #ルーティンエラー
   def destroy
     post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: post_image.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # get 'users/show' コントーラー生成時に作成されるが重複するためコメントアウト
   root "post_images#index"
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show] do #post_imagesとpost_commentsは親子関係
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do #post_imagesとpost_commentsは親子関係
     resources :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy] #親のresocesで指定したコントローラに、子のresorcesで指定したコントローラが続く(「ネスト」すると表記
   end
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
